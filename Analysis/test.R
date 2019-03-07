1e4596ddf0767b54009e88300c936df179a44bf6
Naika Dorilas <ndorilas2014@fau.edu>
2019-03-07 13:40:02 -0700

    Trouble shooting P_x function

--- Analysis/test.R
+++ Analysis/test.R
@@ -1,4 +1,4 @@
-source("R/myRfunctions.R")
+source("../R/myRfunctions.R")
 
 # 
 # 
@@ -49,42 +49,75 @@ source("R/myRfunctions.R")
 # 
 
 
-`
+###Figuring out where error is in P_x
 
-S=50 #matrix size
+S=20 #matrix size
 mu=-1 #mean
 sigma=0.5 #deviation
 d=10 #diagonal elements
-N=50 #number of data points per species
+N=20 #number of data points per species
+e=-1
+muV=seq(-10,10,by=0.5)
+sigmaV=seq(0.1,10, by =0.2)
+dV=seq(0, 10, by=1)
+#ind=numeric(nrow(params))
+params=expand.grid(muV,sigmaV,dV)
 
-A=makeASymmetric(mu=mu,sigma=sigma,d=-d, S=S)
+p=lapply(1:nrow(params), function(i){
+    A=makeASymmetric(mu=params[i,1],sigma=params[i,2],d=params[i,3], S=S)
+    #X=tryCatch(expr=makeX(mu=params[i,1],sigma=params[i,2],d=params[i,3],S=S,N=N))
+    X=try(makeX(mu=params[i,1],sigma=params[i,2],d=params[i,3],S=S,N=N))#createX
+    if(class(X)=='try-error'){
+        return(NA)
+    }else{
+    return(P_x(X,A))
+    }
+    
+})
 
-if(errorND(A)==1)
-{
-    print("Error: The matrix is not diagonally stable")
-} else
-    
-{
-    ###Variables
-    
-    N=500
-    dt=0.01 #time steps
-    y=matrix(0,nrow=S, ncol=N) #population
-    #yint=rnorm(mean=7, sd=3,n=10) #initial condition
-    yint=15
-    
-    
-    
-    y<-createTS(A=A,dt=dt,y0=1,N=N,S=S,sigma=15)
-    
-}
+ind=lapply(1:nrow(params),function(i){
+    if(is.na(as.numeric((p[i])))){
+        print(paste("problem at index: ", i))
+        print(params[i,])
+        return(i)
+    }
+    else{
+        return(0)
+    }
+})
 
-mu=1
-sigma=0.5
-d=10
-S=10
-N=10
-B=100
+# A=makeASymmetric(mu=params[i,1],sigma=params[i,2],d=params[i,3], S=S)
+# X=makeX(mu=params[i,1],sigma=params[i,2],d=params[i,3],S,N)
+# 
+# p[i]=P_x(X,A)
 
-X=makeX(mu,sigma,d,S,N)
-optim(par=c(mu,sigma,d),fn=MonteCarlo,X=X,B=B,method="BFGS")
+# 
+# if(errorND(A)==1)
+# {
+#     print("Error: The matrix is not diagonally stable")
+# } else
+#     
+# {
+#     ###Variables
+#     
+#     N=500
+#     dt=0.01 #time steps
+#     y=matrix(0,nrow=S, ncol=N) #population
+#     #yint=rnorm(mean=7, sd=3,n=10) #initial condition
+#     yint=15
+#     
+#     
+#     
+#     y<-createTS(A=A,dt=dt,y0=1,N=N,S=S,sigma=15)
+#     
+# }
+
+# mu=1
+# sigma=0.5
+# d=10
+# S=10
+# N=10
+# B=100
+# 
+# X=makeX(mu,sigma,d,S,N)
+# optim(par=c(mu,sigma,d),fn=MonteCarlo,X=X,B=B,method="BFGS")
