2fcc2b7adcd554c8daeedc194ee1bfe18d751ea2
Naika Dorilas <ndorilas2014@fau.edu>
2019-03-07 13:41:29 -0700

    optimized .mc2opt function

--- Optimization/Optimization.R
+++ Optimization/Optimization.R
@@ -6,6 +6,6 @@ N=10
 B=100
 
 X=makeX(mu,sigma,d,S,N)
-optim(par=c(mu,sigma,d),fn=MonteCarlo,X=X,B=B, method = 'L-BFGS-B', 
+optim(par=c(mu,sigma,d),fn=.mc2opt,X=X,B=B, method = 'L-BFGS-B', 
       lower = c(-10, 0, 0), upper = c(10, 10, 100),
       control = list(fnscale = -1))
