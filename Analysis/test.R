6061cbc4b33e1ad2faf789998348101650c058b5
ajrominger <ajrominger@gmail.com>
2019-03-08 11:22:12 -0700

    added convergence info to output

--- Analysis/test.R
+++ Analysis/test.R
@@ -136,8 +136,6 @@ params=params[rep(1:nrow(params),5), ]
 nCores = ifelse(grepl('Mac-Pro', Sys.info()['nodename']), 10, 1)
 
 p=parallel::mclapply(1:nrow(params), mc.cores = nCores, FUN = function(i){
-# p=lapply(1:nrow(params), function(i){
-#     browser()
     print(i)
     mu=params[i,1]
     sigma=params[i,2]
@@ -150,11 +148,11 @@ p=parallel::mclapply(1:nrow(params), mc.cores = nCores, FUN = function(i){
     out<-optim(par=c(mu,sigma,d),fn=.mc2opt,X=X,B=B, method = 'L-BFGS-B', 
                 lower = c(-10, 0, 0), upper = c(10, 10, 100),
                 control = list(fnscale = -1))
-    return(out$par)
+    return(c(out$par, out$convergence))
 })
 
 p=do.call(rbind,p)
-colnames(p) = c('mu_est', 'sigma_est', 'd_est')
+colnames(p) = c('mu_est', 'sigma_est', 'd_est', 'convergence')
 
 out = cbind(params, p)
 
