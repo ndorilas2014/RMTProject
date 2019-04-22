7fc72c5ef66d533cd1ffac77568ea018a7b27c1b
Naika Dorilas <ndorilas2014@fau.edu>
2019-04-22 15:52:07 -0400

    added file mcEstimates for Empirical Data

--- inst/Optimization/mcEstimates_symmetricA_mdata.R
+++ inst/Optimization/mcEstimates_symmetricA_mdata.R
@@ -2,13 +2,26 @@ source("R/myRfunctions.R")
 
 load("EmpiricalData/feces_F4.Rdata")
 load("EmpiricalData/feces_M3.Rdata")
-
+load("EmpiricalData/L_palm_F4.Rdata")
+load("EmpiricalData/L_palm_M3.Rdata")
+load("EmpiricalData/R_palm_F4.Rdata")
+load("EmpiricalData/R_palm_M3.Rdata")
+load("EmpiricalData/Tongue_F4.Rdata")
+load("EmpiricalData/Tongue_M3.Rdata")
 
 
 #Normalizing data
 
-a<-t(t(feces_F4)/ colSums(feces_F4))
-#a<-t(t(feces_M3)/ colSums(feces_M3))
+# a<-t(t(feces_F4)/ colSums(feces_F4))
+# a<-t(t(feces_M3)/ colSums(feces_M3))
+# a<-t(t(L_palm_F4)/ colSums(L_palm_F4))
+# a<-t(t(L_palm_M3)/ colSums(L_palm_M3))
+# a<-t(t(R_palm_F4)/ colSums(R_palm_F4))
+# a<-t(t(R_palm_M3)/ colSums(R_palm_M3))
+# a<-t(t(Tongue_F4)/ colSums(Tongue_F4))
+ a<-t(t(Tongue_M3)/ colSums(Tongue_M3))
+
+
 X<-(a-rowMeans(a))/sqrt(rowMeans(a^2)-rowMeans(a)^2)
 
 mu=mean(X)
@@ -17,8 +30,9 @@ d=mean(diag(X))
 S=nrow(X)
 N=ncol(X)
 
-out<-optim(par=c(mu,sigma,d),fn=.mc2opt,X=X,B=B, method = 'L-BFGS-B', 
+optim(par=c(1,2,7),fn=.mc2opt,X=X,B=10, method = 'L-BFGS-B', 
            lower = c(-10, 0, 0), upper = c(10, 10, 100),
            control = list(fnscale = -1))
 out$par
 c(mu,sigma,d)
+out$convergence
