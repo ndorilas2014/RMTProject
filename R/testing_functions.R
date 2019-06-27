67edc50786cf525404e94bca34a2cbdb8f77c3c0
Naika Dorilas <ndorilas2014@fau.edu>
2019-06-27 11:39:46 -0400

    notes for next week, executable rmd file

---
+++ R/testing_functions.R
@@ -0,0 +1,12 @@
+source("R/myRfunctions.R")
+
+#restrict 2 parameters
+MonteCarlo(c(2,1.1,5),makeX(2,1,5,S=10,N=10),B=100)
+
+#plotting likelihood with respect to mu and sigma
+
+#debug code
+
+#calculations
+
+#email within 2 days
